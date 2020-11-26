74cbcccc99ec2a7ace1434a138e7d3a7fdca54c9
yehuda kahan <yehudajka@gmail.com>
2020-11-26 14:56:37 +0200

    Continue AddTravelActivity.kt
    Set validations (AwesomeValidation)

--- app/src/main/java/com/example/travelrequestform/ui/addTravelActivity/AddTravelActivity.kt
+++ app/src/main/java/com/example/travelrequestform/ui/addTravelActivity/AddTravelActivity.kt
@@ -2,8 +2,9 @@ package com.example.travelrequestform.ui.addTravelActivity
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.location.Location
 import android.os.Bundle
-import android.text.InputType
+import android.util.Patterns
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
@@ -11,6 +12,10 @@ import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.basgeekball.awesomevalidation.utility.RegexTemplate
+import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation
 import com.example.travelrequestform.R
 import com.example.travelrequestform.data.models.Travel
 import com.google.android.libraries.places.api.Places
@@ -19,13 +24,14 @@ import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.android.material.button.MaterialButton
+import com.example.travelrequestform.data.models.Travel.UserLocation
 import java.util.*
 
 
 const val ADDRESS_CODE = 1
 const val LOCATION_CODE = 2
 
-class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
+class AddTravelActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var travel: Travel
     private lateinit var travelViewModel: TravelViewModel
@@ -40,14 +46,17 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
     private lateinit var etReturnDate: EditText
     private lateinit var btnSend: MaterialButton
 
+    private lateinit var addressPlace : Place
+    private lateinit var destinationPlace : Place
+    private lateinit var awesomeValidation: AwesomeValidation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_travel)
 
         Places.initialize(applicationContext, "AIzaSyD10IpMmv30oFrH1iwNvsXkjFx7ZuKCSck")
-        travel = Travel()
         initializeViews()
+        setValidation()
         setOnClickListeners()
         initializeSpinner()
 
@@ -62,36 +71,26 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
                     Toast.LENGTH_LONG
                 ).show()
         })
-
-        /*travel.arrivalDate = Date(2020, 12, 4)
-        travel.clientEmail = "yehudajka@gmail.com"
-        travel.clientName = "Yehuda"
-        travel.clientPhone = "055093239"
-        travel.company = HashMap()
-        travel.company["gogo"] = true
-        travel.requestType = RequestType.SENT
-        travel.travelDate = Date(2020, 11, 20)
-        travel.travelLocations.add(UserLocation(1.44, 2.33))
-
-        travelViewModel.addTravel(travel)*/
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == RESULT_OK) {
-            // initialize place
-            val place = Autocomplete.getPlaceFromIntent(data as Intent)
 
             when (requestCode) {
                 ADDRESS_CODE -> {
+                    // initialize place
+                    addressPlace = Autocomplete.getPlaceFromIntent(data as Intent)
                     // set address on EditText
-                    etAddress.setText(place.address)
+                    etAddress.setText(addressPlace.address)
                     //travel.address?.convertFromPlace(place)
                 }
                 LOCATION_CODE -> {
+                    // initialize place
+                    destinationPlace = Autocomplete.getPlaceFromIntent(data as Intent)
                     // set address on EditText
-                    etDestination.setText(place.address)
+                    etDestination.setText(destinationPlace.address)
                     //travel.address?.convertFromPlace(place)
                 }
             }
@@ -116,7 +115,7 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
         etDestination.isFocusable = false
         numOfTravelers = findViewById(R.id.num_of_travelers)
         etTravelDate = findViewById(R.id.et_travelDate)
-        etTravelDate.inputType = InputType.TYPE_NULL
+        //etTravelDate.inputType = InputType.TYPE_NULL
         etReturnDate = findViewById(R.id.et_returnDate)
         btnSend = findViewById(R.id.btn_send)
     }
@@ -145,6 +144,7 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
         etDestination.setOnClickListener(this)
         etTravelDate.setOnClickListener(this)
         etReturnDate.setOnClickListener(this)
+        btnSend.setOnClickListener(this)
     }
 
     private fun initializeSpinner() {
@@ -173,6 +173,7 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
                     month,
                     day
                 )
+                picker.datePicker.minDate = System.currentTimeMillis() - 1000
                 picker.show()
             }
             etReturnDate -> {
@@ -183,6 +184,7 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
                     month,
                     day
                 )
+                picker.datePicker.minDate = System.currentTimeMillis() - 1000
                 picker.show()
             }
         }
@@ -195,6 +197,68 @@ class AddTravelActivity : AppCompatActivity() , View.OnClickListener{
             etDestination -> onClickPlace(v)
             etTravelDate -> onClickDate(v)
             etReturnDate -> onClickDate(v)
+            btnSend -> clickSend()
+        }
+    }
+
+    private fun setValidation() {
+        awesomeValidation = AwesomeValidation(ValidationStyle.BASIC)
+        awesomeValidation.addValidation(
+            this, R.id.et_name,
+            RegexTemplate.NOT_EMPTY, R.string.invalid_name
+        )
+        awesomeValidation.addValidation(
+            this, R.id.et_phone,
+            "^\\+?(972|0)(\\-)?0?(([23489]{1}\\d{7})|[5]{1}\\d{8})\$", R.string.invalid_phone
+        )
+        awesomeValidation.addValidation(
+            this, R.id.et_mail,
+            Patterns.EMAIL_ADDRESS, R.string.invalid_email
+        )
+        awesomeValidation.addValidation(etAddress, SimpleCustomValidation {
+            return@SimpleCustomValidation it.isNotEmpty()
+        }, "אנא בחר כתובת יציאה וחזרה")
+        awesomeValidation.addValidation(
+            etDestination, SimpleCustomValidation {
+                return@SimpleCustomValidation it.isNotEmpty()
+            }, "אנא בחר כתובת יעד"
+        )
+        awesomeValidation.addValidation(etTravelDate, SimpleCustomValidation {
+
+            if (etTravelDate.text.isNotEmpty() && etReturnDate.text.isNotEmpty()) {
+                val travelDate = Date.parse(etTravelDate.text.toString())
+                val returnDate = Date.parse(etReturnDate.text.toString())
+                return@SimpleCustomValidation returnDate - travelDate > 0
+            } else return@SimpleCustomValidation false
+        }, "התאריך חזרה חייב להיות מאוחר מתאריך היציאה")
+
+        awesomeValidation.addValidation(etReturnDate, SimpleCustomValidation {
+
+            if (etTravelDate.text.isNotEmpty() && etReturnDate.text.isNotEmpty()) {
+                val travelDate = Date.parse(etTravelDate.text.toString())
+                val returnDate = Date.parse(etReturnDate.text.toString())
+                return@SimpleCustomValidation returnDate - travelDate > 0
+            } else return@SimpleCustomValidation false
+        }, "התאריך חזרה חייב להיות מאוחר מתאריך היציאה")
+
+    }
+
+    private fun clickSend() {
+        awesomeValidation.clear()
+        if (awesomeValidation.validate()) {
+            travel = Travel()
+            travel.arrivalDate = Date(etReturnDate.text.toString())
+            travel.clientEmail = etMail.text.toString()
+            travel.clientName = etName.text.toString()
+            travel.clientPhone = etPhone.text.toString()
+            travel.company = HashMap()
+            travel.requestType = Travel.RequestType.SENT
+            travel.travelDate = Date(etTravelDate.text.toString())
+            travel.numOfTravelers = numOfTravelers.selectedItem as Int
+            travel.travelLocations.add(UserLocation(destinationPlace))
+            travel.address = UserLocation(addressPlace)
+
+            travelViewModel.addTravel(travel)
         }
     }
 }
